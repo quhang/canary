169b8bbb081f8d1e21824cd346fcc367687ef1c6
Hang Qu(eevee) <quhang@stanford.edu>
2016-09-30 14:35:29 -0700

    Update.

--- app/shuffle/shuffle.cc
+++ app/shuffle/shuffle.cc
@@ -19,14 +19,12 @@ class ShuffleTestApplication : public CanaryApplication {
   // The program.
   void Program() override {
     // Declares variables.
-    auto d_component = DeclareVariable<std::vector<int>>(FLAG_app_partitions);
+    auto d_component = DeclareVariable<int>(FLAG_app_partitions);
 
     WriteAccess(d_component);
     Transform([=](CanaryTaskContext* task_context) {
       auto input = task_context->WriteVariable(d_component);
-      for (int i = 0; i < FLAG_app_partitions; ++i) {
-        input->push_back(i);
-      }
+      *input = task_context->GetPartitionId();
     });
 
     Loop(FLAG_app_iterations);
@@ -34,13 +32,7 @@ class ShuffleTestApplication : public CanaryApplication {
     TrackNeeded();
     ReadAccess(d_component);
     Scatter([=](CanaryTaskContext* task_context) {
-      std::vector<std::vector<int>> buffer(FLAG_app_partitions);
-      for (auto number : task_context->ReadVariable(d_component)) {
-        buffer[number % FLAG_app_partitions].push_back(number);
-      }
-      for (int i = 0; i < FLAG_app_partitions; ++i) {
-        task_context->Scatter(i, buffer[i]);
-      }
+      task_context->Broadcast(task_context->ReadVariable(d_component));
       if (task_context->GetPartitionId() == 0) {
         printf("%.9f\n",
                time::timepoint_to_double(time::WallClock::now()));
@@ -51,13 +43,7 @@ class ShuffleTestApplication : public CanaryApplication {
     WriteAccess(d_component);
     Gather([=](CanaryTaskContext* task_context) -> int {
       EXPECT_GATHER_SIZE(FLAG_app_partitions);
-      auto result = task_context->Gather<std::vector<int>>();
-      std::vector<int> shuffled_result;
-      for (auto& vector : result) {
-        shuffled_result.insert(shuffled_result.end(),
-                               vector.begin(), vector.end());
-      }
-      std::sort(shuffled_result.begin(), shuffled_result.end());
+      auto result = task_context->Gather<int>();
       return 0;
     });
 
