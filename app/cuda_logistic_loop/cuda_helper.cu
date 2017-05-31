87d0e4c8cf0c84ff6c5119849db5d0217c5527e3
Hang Qu(eevee) <quhang@stanford.edu>
2017-05-31 14:11:20 -0700

    Cuda logistic regression done.

--- app/cuda_logistic_loop/cuda_helper.cu
+++ app/cuda_logistic_loop/cuda_helper.cu
@@ -68,46 +68,45 @@ namespace app {
 Eigen::CudaStreamDevice cuda_stream(0);
 Eigen::GpuDevice gpu_device(&cuda_stream);
 
-void GenerateRandomData(const std::vector<double> reference,
-                        GpuTensorStore<double, 2>* x_data,
-                        GpuTensorStore<double, 1>* y_data) {
+void GenerateRandomData(const std::vector<float> reference,
+                        GpuTensorStore<float, 2>* x_data,
+                        GpuTensorStore<float, 1>* y_data) {
   const size_t dim = x_data->get_ranks()[0];
   const size_t samples = x_data->get_ranks()[1];
   if (reference.size() != dim || y_data->get_ranks()[0] != samples) {
     fprintf(stderr, "Dimension mismatch for GPU execution!\n");
     return;
   }
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> x_tensor(
-      (double*)x_data->get_data(), dim, samples);
-  x_tensor.device(gpu_device) = x_tensor.random();
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> y_tensor(
-      (double*)y_data->get_data(), 1, samples);
-  GpuTensorStore<double, 1> w_data(std::array<size_t, 1>{dim});
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> x_tensor(
+      (float*)x_data->get_data(), dim, samples);
+  x_tensor.device(gpu_device) = x_tensor.random() - x_tensor.constant(0.5);
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> y_tensor(
+      (float*)y_data->get_data(), 1, samples);
+  GpuTensorStore<float, 1> w_data;
   w_data.ToDevice(reference);
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> w_tensor(
-      (double*)w_data.get_data(), 1, dim);
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> w_tensor(
+      (float*)w_data.get_data(), 1, dim);
   Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
   y_tensor.device(gpu_device) =
     (w_tensor.contract(x_tensor, product_dims) > y_tensor.constant(0)).select(
         y_tensor.constant(1), y_tensor.constant(-1));
 }
 
-void UpdateWeight(const GpuTensorStore<double, 2>& x_data,
-                  const GpuTensorStore<double, 1>& y_data,
-                  const GpuTensorStore<double, 1>& w_data,
-                  GpuTensorStore<double, 1>* g_data) {
+void UpdateWeight(const GpuTensorStore<float, 2>& x_data,
+                  const GpuTensorStore<float, 1>& y_data,
+                  const GpuTensorStore<float, 1>& w_data,
+                  GpuTensorStore<float, 1>* g_data) {
   const size_t dim = x_data.get_ranks()[0];
   const size_t samples = x_data.get_ranks()[1];
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> x_tensor(
-      (double*)(x_data.get_data()), dim, samples);
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> y_tensor(
-      (double*)(y_data.get_data()), 1, samples);
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> w_tensor(
-      (double*)(w_data.get_data()), 1, dim);
-
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> x_tensor(
+      (float*)(x_data.get_data()), dim, samples);
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> y_tensor(
+      (float*)(y_data.get_data()), 1, samples);
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> w_tensor(
+      (float*)(w_data.get_data()), 1, dim);
   g_data->Resize({dim});
-  Eigen::TensorMap<Eigen::Tensor<double, 2>> g_tensor(
-      (double*)(g_data->get_data()), 1, dim);
+  Eigen::TensorMap<Eigen::Tensor<float, 2>> g_tensor(
+      (float*)(g_data->get_data()), dim, 1);
 
   Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
   auto dot = w_tensor.contract(x_tensor, product_dims);
@@ -116,22 +115,6 @@ void UpdateWeight(const GpuTensorStore<double, 2>& x_data,
       - y_tensor.constant(1.));
   Eigen::array<Eigen::IndexPair<int>, 1> new_dims = { Eigen::IndexPair<int>(1, 1) };
   g_tensor.device(gpu_device) = x_tensor.contract(factor, new_dims);
-
-//      const auto& feature = task_context->ReadVariable(d_feature);
-//      const auto& local_w = task_context->ReadVariable(d_local_w).ToHost();
-//      std::vector<double> local_gradient_buffer(DIMENSION, 0);
-//      for (const auto& pair : feature) {
-//        const Point& point = pair.first;
-//        const bool flag = pair.second;
-//        const auto dot = std::inner_product(local_w.begin(), local_w.end(),
-//                                            point.begin(), 0.);
-//        const double factor =
-//            flag ? +(1. / (1. + std::exp(-dot)) - 1.)
-//                 : -(1. / (1. + std::exp(+dot)) - 1.);
-//        for (int i = 0; i < DIMENSION; ++i) {
-//          local_gradient_buffer[i] += factor * point[i];
-//        }
-//      }
 }
 
 }  // namespace app
--- app/cuda_logistic_loop/cuda_logistic_loop.cc
+++ app/cuda_logistic_loop/cuda_logistic_loop.cc
@@ -24,7 +24,7 @@ class LogisticLoopApplication : public CanaryApplication {
  public:
   // The program.
   void Program() override {
-    typedef std::vector<double> Point;
+    typedef std::vector<float> Point;
     const Point reference{1., -1., 1., 1., -1., 2.,  2.,
                           2., 1.,  1., 0., 1.,  -1., 1.,
                           1., 1.,  2., 1., 1.,  1.};
@@ -32,10 +32,10 @@ class LogisticLoopApplication : public CanaryApplication {
     // Declares variables.
 
     // Placed on a computation node.
-    auto d_x = DeclareVariable<GpuTensorStore<double, 2>>(FLAG_app_partitions);
-    auto d_y = DeclareVariable<GpuTensorStore<double, 1>>();
-    auto d_local_gradient = DeclareVariable<GpuTensorStore<double, 1>>();
-    auto d_local_w = DeclareVariable<GpuTensorStore<double, 1>>();
+    auto d_x = DeclareVariable<GpuTensorStore<float, 2>>(FLAG_app_partitions);
+    auto d_y = DeclareVariable<GpuTensorStore<float, 1>>();
+    auto d_local_gradient = DeclareVariable<GpuTensorStore<float, 1>>();
+    auto d_local_w = DeclareVariable<GpuTensorStore<float, 1>>();
 
     // Placed on a parameter node.
     auto d_global_gradient = DeclareVariable<Point>();
@@ -57,8 +57,6 @@ class LogisticLoopApplication : public CanaryApplication {
       task_context->WriteVariable(d_global_w)->assign(DIMENSION, 1.);
     });
 
-
-
     Loop(FLAG_app_iterations);
 
     TrackNeeded();
@@ -72,7 +70,7 @@ class LogisticLoopApplication : public CanaryApplication {
     Gather([=](CanaryTaskContext* task_context) -> int {
       EXPECT_GATHER_SIZE(1);
       // Receive the global weight and load it into GPU.
-      std::vector<double> buffer;
+      std::vector<float> buffer;
       task_context->GatherSingle(&buffer);
       task_context->WriteVariable(d_local_w)->ToDevice(buffer);
       return 0;
@@ -102,7 +100,7 @@ class LogisticLoopApplication : public CanaryApplication {
       global_gradient->assign(DIMENSION, 0);
       task_context->Reduce(
           global_gradient,
-          [=](const std::vector<double>& left, std::vector<double>* right) {
+          [=](const std::vector<float>& left, std::vector<float>* right) {
             for (int i = 0; i < DIMENSION; ++i) { (*right)[i] += left[i]; }
           });
       return 0;
